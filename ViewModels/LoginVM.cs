9f931aeebf733fe1815e9786623291fc6dfe5a1b
redfoxbg <redfoxbg@gmail.com>
2024-02-07 18:34:07 +0200

    Adding event listener for login

--- ViewModels/LoginVM.cs
+++ ViewModels/LoginVM.cs
@@ -2,7 +2,8 @@
 
 namespace Simple_App_WPF_MVVM.ViewModels
 {
-    internal class LoginVM
+
+    internal class LoginVM : ViewModelBase
     {
         private User user;
         private string userName;
@@ -16,15 +17,21 @@ namespace Simple_App_WPF_MVVM.ViewModels
         public string UserName
         {
             get { return user.UserName; }
-            set { user.UserName = value; }
+            set {
+                user.UserName = value; 
+                OnPropertyChanged(nameof(UserName));
+                }
         }
 
         public string Password
         {
             get { return user.Password; }
-            set { user.Password = value; }
+            set { 
+                user.Password = value;
+                OnPropertyChanged(nameof(Password));
+                }
         }
 
-
+      
     }
 }
---
+++ ViewModels/ViewModelBase.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel;
+
+namespace Simple_App_WPF_MVVM.ViewModels
+{
+    public class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public void OnPropertyChanged(string propertyName)
+            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
