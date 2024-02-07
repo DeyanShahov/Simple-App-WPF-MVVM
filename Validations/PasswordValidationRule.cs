06629ae5762b4c34da0e3d09b2843bbd44fb72a0
redfoxbg <redfoxbg@gmail.com>
2024-02-07 21:01:23 +0200

    Adding custom login validations

---
+++ Validations/PasswordValidationRule.cs
@@ -0,0 +1,23 @@
+﻿using System.Globalization;
+using System.Text.RegularExpressions;
+using System.Windows.Controls;
+
+namespace Simple_App_WPF_MVVM.Validations
+{
+    public class PasswordValidationRule : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            Regex regex = new Regex("^[A-Za-z0-9!@#$%^&]{5,10}$");
+
+            if (!regex.IsMatch(value.ToString()))
+            {
+                return new ValidationResult(false, "Please enter a valid password");
+            }
+            else
+            {
+                return ValidationResult.ValidResult;
+            }
+        }
+    }
+}
---
+++ Validations/UserNameValidationRule.cs
@@ -0,0 +1,29 @@
+﻿using System.Globalization;
+using System.Text.RegularExpressions;
+using System.Windows.Controls;
+
+namespace Simple_App_WPF_MVVM.Validations
+{
+    public class UserNameValidationRule : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            Regex regex = new Regex("^[A-Za-z0-9!@#$%^&]{2,25}$");
+
+            string? input = value.ToString();
+
+            if (!regex.IsMatch(input))
+            {
+                if (input.Length < 2 || input.Length > 25)
+                {
+                    return new ValidationResult(false, "Username have atleast 2 and maximum 25 character");
+                }
+                return new ValidationResult(false, "Please enter a valid username");
+            }
+            else
+            {
+                return ValidationResult.ValidResult;
+            }
+        }
+    }
+}
