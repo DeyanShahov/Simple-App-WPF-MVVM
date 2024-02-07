6f76d01d0ff0850305f2b36e340c069b082cdbc7
redfoxbg <redfoxbg@gmail.com>
2024-02-07 18:09:22 +0200

    Adding User and LoginVM

--- Models/User.cs
+++ Models/User.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Simple_App_WPF_MVVM.Models
+﻿namespace Simple_App_WPF_MVVM.Models
 {
     internal class User
     {
+        public string UserName { get; set; }
+        public string Password { get; set; }
     }
 }
---
+++ ViewModels/LoginVM.cs
@@ -0,0 +1,30 @@
+﻿using Simple_App_WPF_MVVM.Models;
+
+namespace Simple_App_WPF_MVVM.ViewModels
+{
+    internal class LoginVM
+    {
+        private User user;
+        private string userName;
+
+        public LoginVM()
+        {
+            user = new User();
+        }
+
+
+        public string UserName
+        {
+            get { return user.UserName; }
+            set { user.UserName = value; }
+        }
+
+        public string Password
+        {
+            get { return user.Password; }
+            set { user.Password = value; }
+        }
+
+
+    }
+}
